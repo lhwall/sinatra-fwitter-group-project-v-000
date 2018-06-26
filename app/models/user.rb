83b5dc02f925eec7e3845194958f0c92bee29c0d
Lucy Wall <lhwall@gmail.com>
2018-06-26 19:27:28 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
     has_many :tweets
       has_secure_password
-      validates_presence_of :name, :email, :password
+      validates_presence_of :username, :email, :password
 
       def slug
   slug = ""
